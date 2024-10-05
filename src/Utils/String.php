1f10a1045a5d70ef47f42dcd17affce16bf60a3e
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-10-05 15:36:21 +0200

    Added Exceptions class to project and Added few functions to String class

--- src/Utils/String.php
+++ src/Utils/String.php
@@ -1,1 +1,59 @@
 <?php
+
+$ALPHABET = "qwertzuiopasdfghjklyxcvbnm";
+
+function trimLeft(&$string, $character = ' ')
+{
+    $string = ltrim($string, $character);
+}
+
+function trimRight(&$string, $character = ' ')
+{
+    $string = rtrim($string, $character);
+}
+
+function trimBoth(&$string, $character = ' ') {
+    $string = rtrim($string, $character);
+    $string = ltrim($string, $character);
+}
+
+function appendLeft(&$string, $strToAppend, $spaceBetween = FALSE)
+{
+    if ($spaceBetween) {
+        $string = $strToAppend . " " . $string;
+    } else {
+        $string = $strToAppend . $string;
+    }
+}
+
+function appendRight(&$string, $strToAppend, $spaceBetween = FALSE)
+{
+    if ($spaceBetween) {
+        $string = $string . " " . $strToAppend;
+    } else {
+        $string = $string . $strToAppend;
+    }
+}
+
+function toCharArray($string) {
+    if ($string != NULL && strlen($string) > 0) {
+        return str_split($string);
+    }
+
+    return NULL;
+}
+
+function hasOnlyAlphabet($string)
+{
+    global $ALPHABET;
+
+    $letters = str_split($string);
+
+    foreach ($letters as $letter) {
+        if (!strpos($ALPHABET, strtolower($letter)) && $letter != " ") {
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
