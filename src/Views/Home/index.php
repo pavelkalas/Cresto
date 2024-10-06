d11bb4bdf513c2fc04812fd575f20e44a4117664
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-10-06 18:02:16 +0200

    Little changed default webpage and added one more message

--- src/Views/Home/index.php
+++ src/Views/Home/index.php
@@ -7,7 +7,14 @@
         </h1>
 
         <p>
-            Cresto is a lightweight, straightforward PHP framework built to simplify web development. It’s designed around a clean folder structure, allowing you to easily manage pages.
+            Cresto is a lightweight, straightforward PHP framework built to simplify web development. It’s designed
+            around a clean folder structure, allowing you to easily manage pages.
+        </p>
+
+        <br>
+
+        <p style="color: #ffff00;">
+            This is a Cresto default webpage, just for testing if everything works fine.
         </p>
     </div>
 
